1bd4ba0c2867268e20711e3b344a774578e41bb6
Christian <christian.sy@hotmail.com>
2018-03-31 14:37:28 -0700

    Initialization of project gradle build

---
+++ FGODamage/app/src/main/java/com/example/christian/fgodamage/LoadServantInfo.java
@@ -0,0 +1,19 @@
+package com.example.christian.fgodamage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Christian on 3/31/2018.
+ */
+
+public class LoadServantInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enemy_select);
+    }
+
+
+}
